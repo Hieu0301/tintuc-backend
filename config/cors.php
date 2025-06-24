eb60ba8df02121c76d75f06273128fdfe68cd89b
Hieu0301 <phamminhhieu.03012002@gmail.com>
2025-06-24 10:48:08 +0700

    e-3

--- config/cors.php
+++ config/cors.php
@@ -3,8 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    // 'allowed_origins' => ['https://tintuc-frontend.vercel.app'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tintuc-frontend.vercel.app'],
     'allowed_headers' => ['*'],
-    'supports_credentials' => false,
+    'supports_credentials' => true, // ✅ Rất quan trọng nếu frontend gửi cookie/token
 ];
