d23c6c65cfac3b1e57089a01e7ae689987159221
Hieu0301 <phamminhhieu.03012002@gmail.com>
2025-06-24 10:58:05 +0700

    e-5

--- config/cors.php
+++ config/cors.php
@@ -5,5 +5,5 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://tintuc-frontend.vercel.app'],
     'allowed_headers' => ['*'],
-    'supports_credentials' => true, // ✅ Rất quan trọng nếu frontend gửi cookie/token
+    'supports_credentials' => false,
 ];
