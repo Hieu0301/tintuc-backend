d126ea4292d9042bfed94876254fc02cd7d9d141
Hieu0301 <phamminhhieu.03012002@gmail.com>
2025-06-24 10:28:43 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -8,36 +8,36 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Artisan;
 
-// Route::apiResource('categories', CategoryController::class);
-// Route::apiResource('articles', ArticleController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('articles', ArticleController::class);
 
-// Route::get('/categories/{id}/articles', [CategoryController::class, 'articles']);
-// Route::post('/upload-image', [UploadController::class, 'uploadImage']);
-// Route::get('/test-mail', function () {
-//     Mail::raw('Đây là email test', function ($message) {
-//         $message->to('diachi@example.com')
-//             ->subject('Test gửi mail từ Laravel');
-//     });
+Route::get('/categories/{id}/articles', [CategoryController::class, 'articles']);
+Route::post('/upload-image', [UploadController::class, 'uploadImage']);
+Route::get('/test-mail', function () {
+    Mail::raw('Đây là email test', function ($message) {
+        $message->to('diachi@example.com')
+            ->subject('Test gửi mail từ Laravel');
+    });
 
-//     return 'Đã gửi!';
-// });
+    return 'Đã gửi!';
+});
 
 
-// Route::post('/subscribe', [NewsletterController::class, 'subscribe']);
+Route::post('/subscribe', [NewsletterController::class, 'subscribe']);
 
-// Route::get('/test', function () {
-//     return response('Laravel is OK', 200);
-// });
+Route::get('/test', function () {
+    return response('Laravel is OK', 200);
+});
 
-// Route::get('/run-key', function () {
-//     Artisan::call('key:generate');
-//     return response()->json(['message' => 'APP_KEY generated!']);
-// });
+Route::get('/run-key', function () {
+    Artisan::call('key:generate');
+    return response()->json(['message' => 'APP_KEY generated!']);
+});
 
-// Route::get('/run-migrate', function () {
-//     Artisan::call('migrate', ['--force' => true]);
-//     return response()->json(['message' => 'Migration done!']);
-// });
+Route::get('/run-migrate', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return response()->json(['message' => 'Migration done!']);
+});
 
 
 Route::get('/ping', function () {
