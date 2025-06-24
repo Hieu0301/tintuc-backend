2e458e1bec57d4db78bc925724ac03164d264d4d
Hieu0301 <phamminhhieu.03012002@gmail.com>
2025-06-24 11:18:27 +0700

    i-1

--- config/cors.php
+++ config/cors.php
@@ -5,5 +5,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://tintuc-frontend.vercel.app'],
     'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => false,
 ];
