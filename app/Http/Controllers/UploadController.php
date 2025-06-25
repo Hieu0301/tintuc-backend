1167fc12e736d325a595abb0c3fb76d80de3a5ee
Hieu0301 <phamminhhieu.03012002@gmail.com>
2025-06-25 14:28:10 +0700

    da

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,17 +8,37 @@ use Illuminate\Http\Request;
 
 class UploadController extends Controller
 {
+    // public function uploadImage(Request $request)
+    // {
+    //     if ($request->hasFile('image')) {
+    //         $file = $request->file('image');
+    //         $fileName = time() . '-' . $file->getClientOriginalName();
+
+    //         $path = $file->storeAs('content-images', $fileName, 'public');
+
+    //         return response()->json([
+    //             'success' => true,
+    //             'url' => asset('storage/' . $path),
+    //         ]);
+    //     }
+
+    //     return response()->json(['success' => false, 'message' => 'Không có ảnh']);
+    // }
+
+
     public function uploadImage(Request $request)
     {
         if ($request->hasFile('image')) {
             $file = $request->file('image');
             $fileName = time() . '-' . $file->getClientOriginalName();
 
-            $path = $file->storeAs('content-images', $fileName, 'public');
+            // ✅ Lưu trực tiếp vào public/content-images (KHÔNG dùng storage)
+            $file->move(public_path('content-images'), $fileName);
 
+            // ✅ Trả về URL đúng public path
             return response()->json([
                 'success' => true,
-                'url' => asset('storage/' . $path),
+                'url' => asset('content-images/' . $fileName),
             ]);
         }
 
